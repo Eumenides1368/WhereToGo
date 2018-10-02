9c6dcd4baf566dc54af12b14f4e2f9e635e55340
Eumenides1368 <1271013842@qq.com>
2018-10-02 10:51:18 +0800

    add header

---
+++ src/main.js
@@ -0,0 +1,21 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import 'styles/reset.css'
+import 'styles/border.css'
+import fastClick from 'fastClick'
+// 解决移动端点击300毫秒延迟的问题
+import 'styles/iconfont.css'
+
+Vue.config.productionTip = false
+fastClick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home/components/header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont">&#xe624;</span>
+    </div>
+    <div class="header-middle">
+      <span class="iconfont">&#xe632;</span>输入城市/景点</div>
+    <div class="header-right">
+      <span class="iconfont">&#xe62d;</span>
+      城市
+      </div>
+  </div>
+  <!-- 放在css里面的实在不知怎么注释 @import '~@/assets/styles/variable.styl' @表示src文件 -->
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variable.styl'
+  .header
+    line-height: .86rem
+    background: $bgColor
+    color #ffffff
+    display flex
+    .header-left
+      width .64rem
+      float left
+      .iconfont
+        margin-left .1rem
+    .header-middle
+      margin-top .12rem
+      margin-left .1rem
+      margin-right .12rem
+      height .64rem
+      line-height .64rem
+      border-radius .1rem
+      background #ffffff
+      color #cccccc
+      flex 1
+      .iconfont
+        margin-left .1rem
+        margin-right .1rem
+    .header-right
+      width 1.24rem
+      text-align center
+      padding-right .15rem
+      .iconfont
+        font-size .2rem
+
+</style>
