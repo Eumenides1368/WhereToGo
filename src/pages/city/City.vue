904fef55b01119c6f764a9c7f3df4afdfe4cf63c
Eumenides1368 <1271013842@qq.com>
2018-10-03 23:08:59 +0800

    add city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,18 +1,60 @@
 <template>
   <div>
     <city-header></city-header>
-    <city-search></city-search>
+    <city-search :cities="cities"></city-search>
+    <city-list
+      :cities="cities"
+      :hot="hotCities"
+      :letter="letter"
+    ></city-list>
+    <city-alphabet
+      :cities="cities"
+      @change="handleLetterClick"
+    ></city-alphabet>
   </div>
 </template>
 
 <script>
 import CityHeader from './components/Header'
 import CitySearch from './components/Search'
+import CityList from './components/List'
+import CityAlphabet from './components/Alphabet'
+import axios from 'axios'
+
 export default {
   name: 'city',
   components: {
     CityHeader: CityHeader,
-    CitySearch: CitySearch
+    CitySearch: CitySearch,
+    CityList: CityList,
+    CityAlphabet: CityAlphabet
+  },
+  data () {
+    return {
+      cities: {},
+      hotCities: [],
+      letter: ''
+    }
+  },
+  methods: {
+    getCityInfo () {
+      axios.get('/api/city.json')
+        .then(this.handleGetCityInfoSuccess)
+    },
+    handleGetCityInfoSuccess (res) {
+      res = res.data
+      if (res.ret && res.data) {
+        const data = res.data
+        this.cities = data.cities
+        this.hotCities = data.hotCities
+      }
+    },
+    handleLetterClick (letter) {
+      this.letter = letter
+    }
+  },
+  mounted () {
+    this.getCityInfo()
   }
 }
 </script>
---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area border-topbottom">
+        <div class="title">您的位置</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area border-topbottom">
+        <div class="title">热门城市</div>
+        <div class="content">
+          <div class="button-list">
+            <div class="button-wrapper" v-for="item of hot" :key="item.id">
+              <div class="button">{{ item.name }}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="area border-topbottom"
+        v-for="(item, key) of cities"
+        :key="key"
+        :ref="key">
+        <div class="title">{{ key }}</div>
+        <div class="item-list">
+          <div class="item border-bottom" v-for="innerItem of item" :key="innerItem.id">{{ innerItem.name }}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  props: {
+    hot: Array,
+    cities: Object,
+    letter: String
+  },
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        this.scroll.scrollToElement(element)
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/variable.styl'
+  .border-topbottom
+    &:before
+      color #cccccc
+    &:after
+      color #cccccc
+  .border-bottom
+    &:before
+      color #cccccc
+  // .area
+  //   height .44rem
+  //   background #eeeeee
+  .list
+    overflow hidden
+    position absolute
+    top 1.58rem
+    left 0
+    right 0
+    bottom 0
+  .title
+    background #eeeeee
+    padding-left .1rem
+    color #777777
+    line-height .44rem
+    font-size .26rem
+  .button-list
+    padding .1rem .6rem .1rem .1rem
+    overflow hidden
+    .button-wrapper
+      width 33.33%
+      float left
+      .button
+        margin .1rem
+        padding .1rem 0
+        text-align center
+        border .02rem solid #cccccc
+        border-radius .06rem
+  .item-list
+    .item
+      padding-left .2rem
+      height .64rem
+      line-height .64rem
+</style>
