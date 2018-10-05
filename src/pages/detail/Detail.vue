602d094631c1aa30a62daed5e895388a045e3323
Eumenides1368 <1271013842@qq.com>
2018-10-05 13:20:42 +0800

    add city header

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -1,12 +1,17 @@
 <template>
-  <detail-banner></detail-banner>
+  <div>
+    <detail-header></detail-header>
+    <detail-banner></detail-banner>
+  </div>
 </template>
 
 <script>
 import DetailBanner from './components/Banner'
+import DetailHeader from './components/Header'
 export default {
   name: 'Detail',
   components: {
+    DetailHeader: DetailHeader,
     DetailBanner: DetailBanner
   }
 }
